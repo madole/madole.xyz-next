b6dff36b605e0ed5dd9ed1f6a79d287f74e63b8f
Andrew McDowell <madoliole@gmail.com>
2021-12-27 21:52:36 +1100

    Feat: Nav bar on 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Layout } from "../components/Layout";
 
 const NotFound = () => {
   return (
-    <div className="flex justify-center items-center h-screen flex-col text-white">
-      <h1 className="text-5xl">4 Oooooh 4</h1>
-      <h2 className="text-3xl italic font-thin">Page not found</h2>
-    </div>
+    <Layout>
+      <div className="flex justify-center items-center h-screen flex-col text-white">
+        <h1 className="text-5xl">4 Oooooh 4</h1>
+        <h2 className="text-3xl italic font-thin">Page not found</h2>
+      </div>
+    </Layout>
   );
 };
 
