6ae7a136f86482468e17a1bffc34e9641875d4b4
Andrew McDowell <madoliole@gmail.com>
2024-07-21 17:05:47 +1000

    Added: Suspense to earth

--- components/EarthCanvas.tsx
+++ components/EarthCanvas.tsx
@@ -1,31 +1,33 @@
 import { Canvas } from "@react-three/fiber";
-import React from "react";
+import React, { Suspense } from "react";
 import Earth from "./Earth";
 import { OrbitControls } from "@react-three/drei";
 
 const EarthCanvas: React.FC = () => {
   return (
-    <div className="md:absolute bottom-10 right-0 md:right-10 animate-slowFadeIn z-20 motion-reduce:hidden h-64 w-full md:w-64 cursor-pointer active:cursor-grabbing">
-      <Canvas gl={{ alpha: true }} camera={{ position: [0, 0, 2] }}>
-        <OrbitControls
-          autoRotate={true}
-          autoRotateSpeed={0.2}
-          enableRotate
-          enableZoom={false}
-        />
-        <hemisphereLight intensity={0.85} />
-        <ambientLight intensity={1} />
-        <spotLight
-          position={[-200, 100, 50]}
-          angle={0.3}
-          penumbra={0.5}
-          intensity={0.8}
-          castShadow
-          color={"white"}
-        />
-        <Earth />
-      </Canvas>
-    </div>
+    <Suspense fallback={null}>
+      <div className="md:absolute bottom-10 right-0 md:right-10 animate-slowFadeIn z-20 motion-reduce:hidden h-64 w-full md:w-64 cursor-pointer active:cursor-grabbing">
+        <Canvas gl={{ alpha: true }} camera={{ position: [0, 0, 2] }}>
+          <OrbitControls
+            autoRotate={true}
+            autoRotateSpeed={0.2}
+            enableRotate
+            enableZoom={false}
+          />
+          <hemisphereLight intensity={0.85} />
+          <ambientLight intensity={1} />
+          <spotLight
+            position={[-200, 100, 50]}
+            angle={0.3}
+            penumbra={0.5}
+            intensity={0.8}
+            castShadow
+            color={"white"}
+          />
+          <Earth />
+        </Canvas>
+      </div>
+    </Suspense>
   );
 };
 
